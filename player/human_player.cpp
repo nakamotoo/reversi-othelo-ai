fa21e9f770a6a9267dcebcf1d832907d6e8a5f9a
nakamoto <mitsuhiko0820.acm@gmail.com>
2021-04-13 15:44:45 +0900

    first commit

--- player/human_player.cpp
+++ player/human_player.cpp
@@ -4,36 +4,33 @@
 
 #include "human_player.hpp"
 
-namespace reversi
-{
+namespace reversi {
 
-CellPosition HumanPlayer::thinkNextMove(const Board& board)
-{
-	while (true) {
-		std::cout << "Where? ";
-		std::string position_str;
-		std::cin >> position_str;
-		try {
-		  std::cout << "本当に" << position_str << "でいいですか？(yes/no)\n" << std::endl;
-		  std::string l1;
-		  std::cin >> l1;
-		  std::string l2 = "yes";
-		  if(l1 == l2){
-			auto move = parseCellPosition(position_str);
-			if (board.isLegalMove(move, getSide())) {
-				return move;
-			}
-			std::cout << position_str << " is not a legal move!\n"
-			          << std::endl;
-		  }else{
-		    ;
-		  }
-		} catch (const std::invalid_argument& ex) {
-			std::cout << ex.what() << "\n"
-			          << std::endl;
-			continue;
-		}
-	}
+CellPosition HumanPlayer::thinkNextMove(const Board &board) {
+  while (true) {
+    std::cout << "Where? ";
+    std::string position_str;
+    std::cin >> position_str;
+    try {
+      std::cout << "本当に" << position_str << "でいいですか？(y/n)\n"
+                << std::endl;
+      std::string l1;
+      std::cin >> l1;
+      std::string l2 = "y";
+      if (l1 == l2) {
+        auto move = parseCellPosition(position_str);
+        if (board.isLegalMove(move, getSide())) {
+          return move;
+        }
+        std::cout << position_str << " is not a legal move!\n" << std::endl;
+      } else {
+        ;
+      }
+    } catch (const std::invalid_argument &ex) {
+      std::cout << ex.what() << "\n" << std::endl;
+      continue;
+    }
+  }
 }
 
-}  // namespace reversi
+} // namespace reversi
